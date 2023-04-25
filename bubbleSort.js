b97808a7db5328af37826dc08a0cf995deeb6c20
keviniporre <keviniporres@gmail.com>
2023-04-25 00:36:45 -0400

    Update bubbleSort.js
    Updated Code and Added comments

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,18 +1,24 @@
+//Sample Array
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
+//Declare Bubble Sort Function
 function bubbleSort(array) {
     const length = array.length;
-    for (let j = 0; j < length; j++) {
-        for (let i = 0; i < length; i++){
-        if (array[i]>array[i+1]){
-            let temp = array[i];
-            array[i] = array[i+1];
-            array[i+1] = temp;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++){
+        if (array[j]>array[j + 1]){
+            let temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
             }
         }
-        console.log(numbers);
     }
 }
 
+//Print Original Array
+console.log("UNSORTED ARRAY:")
+console.log(numbers);
+//Execute Bubble Sort Function
+console.log("SORTED ARRAY:")
 bubbleSort(numbers);
 console.log(numbers);
