3e400edc53c943e6ab95b70ed103b47d1963d8df
keviniporre <keviniporres@gmail.com>
2023-04-25 01:20:51 -0400

    Create selectionSort.js
    Selection Sort Algorithm

---
+++ selectionSort.js
@@ -0,0 +1,27 @@
+//Sample Array
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+//Declare Selection Sort Function
+function selectionSort(array) {
+    const length = array.length;
+    let temp = 0;
+
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (array[j] < array[i]) {
+                //swap array[i] and array[j ]
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+//Print Original Array
+console.log("UNSORTED ARRAY:")
+console.log(numbers);
+//Execute Bubble Sort Function
+console.log("SORTED ARRAY (Selection Sort):")
+console.log(selectionSort(numbers));
